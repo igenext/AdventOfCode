0d8da7fdf01a47e63e39e696ce363813a408dcdd
inbarasan <inbarasaninba8@gmail.com>
2022-12-18 18:36:53 +0530

    Added 6th Problem

---
+++ 2022/6/part2.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.util.*;
+
+class part2{
+
+    public static void main(String[] args) {
+        File file = new File("./input.txt");
+        Set<Character> hs = new HashSet<>();
+        int result = 0;
+        try{
+            Scanner sc = new Scanner(file);
+            while(sc.hasNextLine()){
+                String data = sc.nextLine();
+                hs.clear();
+                int left = 0, right = 0;
+                while(true){
+                    if(hs.size()==14){
+                        result = left+14;
+                        break;
+                    }
+                    if(hs.contains(data.charAt(left+right))){
+                        hs.clear();
+                        left++;
+                        right=0;
+                    }
+                    hs.add(data.charAt(left+right));
+                    right++;
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        System.out.println(result);
+    }
+}
